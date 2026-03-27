ddb16e42877843c2996c601574b3ab4266bd6507
leadpipelicker <leadpipelicker@atomicmail.io>
2026-03-27 02:00:29 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+// server.js
 const express = require('express');
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.use(express.json({ limit: '10mb' }));
 
